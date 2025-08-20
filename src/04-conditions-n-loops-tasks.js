ddc6ee37ae17f93dc729193c222ba43454f9339a
FlyaginAS <rus.reg.36@gmail.com>
2025-08-20 11:37:22 +0600

    feat: implement isBracketsBalanced

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -400,8 +400,45 @@ function getDigitalRoot(num) {
  *   '{)' = false
  *   '{[(<{[]}>)]}' = true
  */
-function isBracketsBalanced(/* str */) {
-  throw new Error('Not implemented');
+function isBracketsBalanced(str) {
+  // (),{},[].
+  const stack = [];
+  const dict = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+    '>': '<',
+  };
+  const openBrackets = ['(', '[', '{', '<'];
+  const closeBrackets = [')', ']', '}', '>'];
+  let bracketsAreRight = true;
+
+  function isOpenBracket(char) {
+    return openBrackets.indexOf(char) > -1;
+  }
+  function isCloseBracket(char) {
+    return closeBrackets.indexOf(char) > -1;
+  }
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (isOpenBracket(str[i])) {
+      stack.push(str[i]);
+    } else if (isCloseBracket(str[i])) {
+      //  проверяеям что стек не пуст перед pop()
+      if (stack.length === 0 || dict[str[i]] !== stack.at(-1)) {
+        bracketsAreRight = false;
+        break;
+      }
+      stack.pop();
+    }
+  }
+
+  //  возвращаем true только если стек пуст и нет ошибок
+  if (stack.length > 0) {
+    bracketsAreRight = false;
+  }
+
+  return bracketsAreRight;
 }
 
 
